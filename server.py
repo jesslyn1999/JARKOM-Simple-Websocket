04cc8d3be5b8112a94c0130ee4647265a0eba786
Jesslyn Nathania <jesslynnathania99@gmail.com>
2019-11-11 14:48:51 +0700

    test case 1 done

--- server.py
+++ server.py
@@ -324,7 +324,7 @@ def try_decode_UTF8(data):
 # Called for every client connecting (after handshake)
 def new_client(client, server):
     print("New client connected and was given id %d" % client['id'])
-    server.send_message_to_all("Hey all, a new client has joined us")
+    # server.send_message_to_all("Hey all, a new client has joined us")
 
 
 # Called for every client disconnecting
